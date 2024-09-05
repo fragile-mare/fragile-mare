7092dcbb6cba86f22d15282be5f6b84d276310f9
Kugeki <54151036+Kugeki@users.noreply.github.com>
2024-09-05 22:48:39 +0500

    feat: add start point

--- FM.Unity/fragile-mare/Assets/_Project/Code/Infrastructure/States/GameStates/LoadLevelState.cs
+++ FM.Unity/fragile-mare/Assets/_Project/Code/Infrastructure/States/GameStates/LoadLevelState.cs
@@ -1,12 +1,13 @@
 ﻿using _Project.Code.Infrastructure.Scenes;
+using UnityEngine;
 
 namespace _Project.Code.Infrastructure.States.GameStates
 {
     public class LoadLevelState : IState
     {
+        private const string CharacterPath = "Character/character";
         private readonly ISceneLoader _sceneLoader;
-
-
+        
         public LoadLevelState(ISceneLoader sceneLoader)
         {
             _sceneLoader = sceneLoader;
@@ -14,12 +15,33 @@ namespace _Project.Code.Infrastructure.States.GameStates
 
         public void Enter()
         {
-            _sceneLoader.Load(ScenesNames.Desert);
+            _sceneLoader.Load(ScenesNames.Desert, OnLevelLoaded);
         }
-
+        
         public void Exit()
         {
             
+        }
+
+        public void OnLevelLoaded()
+        {
+            CreateCharacter();
+        }
+
+        public void CreateCharacter()
+        {
+            var initialPosition = Vector3.zero;
+            var initialRotation = Quaternion.identity;
+            
+            var startPoint = GameObject.FindWithTag("StartPoint");
+            if (startPoint != null)
+            {
+                initialPosition = startPoint.transform.position;
+                initialRotation = startPoint.transform.rotation;
+            }
+            
+            var characterPrefab = Resources.Load<GameObject>(CharacterPath);
+            Object.Instantiate(characterPrefab, initialPosition, initialRotation);
         }
     }
 }
