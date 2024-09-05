ab8edaacd9c6687c5250490f227c6b0a2d467f44
Kugeki <54151036+Kugeki@users.noreply.github.com>
2024-09-05 22:11:47 +0500

    refactor: move DI bindings into functions

--- FM.Unity/fragile-mare/Assets/_Project/Code/Infrastructure/Installers/BootstrapInstaller.cs
+++ FM.Unity/fragile-mare/Assets/_Project/Code/Infrastructure/Installers/BootstrapInstaller.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections;
-using _Project.Code.Common.Services.Time;
+﻿using _Project.Code.Common.Services.Time;
 using _Project.Code.Gameplay.Cameras.Providers;
 using _Project.Code.Gameplay.Input.Axis.Services;
 using _Project.Code.Gameplay.Input.Button.Services;
 using _Project.Code.Infrastructure.Scenes;
-using _Project.Code.Infrastructure.States;
 using _Project.Code.Infrastructure.States.Factory;
 using _Project.Code.Infrastructure.States.GameStates;
 using _Project.Code.Infrastructure.States.StateMachine;
@@ -17,18 +15,39 @@ namespace _Project.Code.Infrastructure.Installers
     {
         public override void InstallBindings()
         {
-            Container.BindInterfacesTo<BootstrapInstaller>().FromInstance(this).AsSingle();
-            Container.BindInterfacesAndSelfTo<GameStateMachine>().AsSingle();
-
-            Container.Bind<GameContext>().FromInstance(Contexts.sharedInstance.game);
-            Container.Bind<ISystemsFactory>().To<SystemsFactory>().AsSingle();
-
-            Container.Bind<IStateFactory>().To<StateFactory>().AsSingle();
-
-            Container.Bind<ISceneLoader>().To<SceneLoader>().AsSingle();
-            
-            BindServices();
+            BindInstallers();
+            BindStateMachine();
+            BindContexts();
+            BindFactories();
+            BindLoaders();
             BindGameStates();
+            BindServices();
+        }
+
+        private void BindInstallers()
+        {
+            Container.BindInterfacesTo<BootstrapInstaller>().FromInstance(this).AsSingle();
+        }
+
+        private void BindStateMachine()
+        {
+            Container.BindInterfacesAndSelfTo<GameStateMachine>().AsSingle();
+        }
+
+        private void BindContexts()
+        {
+            Container.Bind<GameContext>().FromInstance(Contexts.sharedInstance.game);
+        }
+
+        private void BindFactories()
+        {
+            Container.Bind<ISystemsFactory>().To<SystemsFactory>().AsSingle();
+            Container.Bind<IStateFactory>().To<StateFactory>().AsSingle();
+        }
+
+        private void BindLoaders()
+        {
+            Container.Bind<ISceneLoader>().To<SceneLoader>().AsSingle();
         }
 
         private void BindGameStates()
