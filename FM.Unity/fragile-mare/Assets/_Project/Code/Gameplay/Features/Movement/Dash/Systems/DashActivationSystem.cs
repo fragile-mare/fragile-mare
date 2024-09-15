d647c09605f1890607628a156e995cf8ae9a6a49
Kugeki <54151036+Kugeki@users.noreply.github.com>
2024-09-15 19:54:09 +0500

    fix(dash): dont use dash when not moving

--- FM.Unity/fragile-mare/Assets/_Project/Code/Gameplay/Features/Movement/Dash/Systems/DashActivationSystem.cs
+++ FM.Unity/fragile-mare/Assets/_Project/Code/Gameplay/Features/Movement/Dash/Systems/DashActivationSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Entitas;
+using UnityEngine;
 
 namespace _Project.Code.Gameplay.Features.Movement.Dash.Systems
 {
@@ -23,6 +24,11 @@ namespace _Project.Code.Gameplay.Features.Movement.Dash.Systems
         {
             foreach (GameEntity dasher in _dashers.GetEntities(_buffer))
             {
+                if (dasher.hasDirection && dasher.Direction == Vector2.zero)
+                {
+                    continue;
+                }
+                
                 if (dasher.hasDashCurrentCount)
                 {
                     var curr = dasher.DashCurrentCount;
